bdec0ccb081b5f7e85e284e63dc6decefb042e03
Gevik Babakhani <gevik@blendjs.com>
2022-06-17 13:22:39 +0200

    fix: savepoint (Fri 17 Jun 2022 13:22:39 CEST)

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git commit -am"fix: savepoint (${1:-`date`})" && \
-git pull --rebase && \
-git add . && \
+git commit -am"fix: savepoint (${1:-`date`})"
+git pull --rebase
+git add
 git push

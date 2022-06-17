9c96b5ef0938852ae7433823c5b901f2eeb139a0
Gevik Babakhani <gevik@blendjs.com>
2022-06-18 00:45:38 +0200

    fix: savepoint (fix command)

---
+++ packages/proxybuilder/src/commands/renew.ts
@@ -0,0 +1,39 @@
+import { CommandBuilder } from "yargs";
+import { ProxyBuilder } from "../builder";
+
+/**
+ * The name of this command
+ */
+export const command = "renew";
+/**
+ * The description of this command
+ */
+export const desc = "Renews an existing certificate";
+/**
+ * The description of command options
+ */
+export const builder: CommandBuilder = {
+    t: {
+        alias: "target",
+        type: "string",
+        description: "Target folder to install the files",
+        default: "proxybuilder"
+    },
+    d: {
+        alias: "domain",
+        type: "array",
+        description: "Domain(s) to be added to the proxy",
+        default:[]
+    },
+    x: {
+        alias: "staging",
+        required: false,
+        type: "boolean",
+        description: "Use LetEncrypt staging"
+    }
+};
+
+export const handler = (argv:any) => {
+    const builder = new ProxyBuilder(argv.target,argv.staging ? true : false)
+    builder.renew(argv.domain)
+};

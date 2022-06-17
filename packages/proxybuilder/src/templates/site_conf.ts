726a702ff7216cabb3bdcba9ba8930013018fbf7
Gevik Babakhani <gevik@blendjs.com>
2022-06-17 17:50:00 +0200

    fix: typo

--- packages/proxybuilder/src/templates/site_conf.ts
+++ packages/proxybuilder/src/templates/site_conf.ts
@@ -34,7 +34,7 @@ server {
 
         location / {
           default_type text/plain;
-          return 200 '${domain}!'";
+          return 200 '${domain}!';
         }
 
         include ${proxyFolder}/security.conf;

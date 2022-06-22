176d16236992577bac084f6867deb17622f9dc58
Gevik Babakhani <gevik@blendjs.com>
2022-06-22 10:13:11 +0200

    fix: savepoint (Wed 22 Jun 2022 10:13:11 CEST)

--- packages/proxybuilder/src/builder.ts
+++ packages/proxybuilder/src/builder.ts
@@ -70,7 +70,7 @@ export class ProxyBuilder {
 
     protected renderProxyConfigs() {
         this.varLetsEncryptFolder = this.initFolder(["var", "letsencrypt"]);
-        ["general.conf", "security.conf", "proxy.conf", "letsencrypt.conf"].forEach((conf) => {
+        ["general.conf", "proxy.conf", "letsencrypt.conf"].forEach((conf) => {
             renderTemplate(
                 conf,
                 {
@@ -153,7 +153,21 @@ export class ProxyBuilder {
             },
             mainConf
         );
-        renderTemplate("entry.conf", { proxyFolder: this.proxyFolder, domain }, path.join(appFolder, "main.conf"));
+        ["site-main.conf", "site-ssl.conf", "site-log.conf", "site-security.conf", "site-custom.conf"].forEach(
+            (conf) => {
+                renderTemplate(
+                    conf,
+                    {
+                        domain,
+                        appFolder: this.appsFolder,
+                        sslFolder: this.sslFolder,
+                        logFolder: this.logsFolder,
+                        proxyFolder: this.proxyFolder
+                    },
+                    path.join(this.appsFolder, domain, conf.replace("site-", ""))
+                );
+            }
+        );
         this.nginxReload();
     }
 
